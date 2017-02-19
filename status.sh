c7aeaddbd8332b7558b6c232637d3b9a432dc0f3
Rimgaila Mantas <JointDee@gmail.com>
2017-02-19 10:31:53 +0200

    Delete status.sh

--- status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-free -m | awk 'NR==2{printf "M: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-df -h | awk '$NF=="/"{printf "D: %d/%dGB (%s)\n", $3,$2,$5}'
-top -bn1 | grep load | awk '{printf "C: %.2f\n", $(NF-2)}'
-systemctl status nginx | awk 'Active'
-echo --------
-who
-echo --------
-~                 
